c2ee7afc3f1cb87ea37a1c46d3b750ae641b4ca2
Mayank Raj <mayankraj052@gmail.com>
2024-08-29 14:15:54 +0530

    time.js problem solved and all test cases run successfully

--- week-2/week-2-js/medium/times.js
+++ week-2/week-2-js/medium/times.js
@@ -8,6 +8,18 @@ Hint - use Date class exposed in JS
 There is no automated test for this one, this is more for you to understand time goes up as computation goes up
 */
 
+
 function calculateTime(n) {
-    return 0.01;
+    const startTime = new Date().getTime();
+
+    let sum = 0;
+    for (let i = 1; i <= n; i++) {
+        sum += i;
+    }
+
+    const endTime = new Date().getTime();
+    const timeTaken = (endTime - startTime) / 1000; // Convert milliseconds to seconds
+
+    return timeTaken;
 }
+console.log(calculateTime(99900));
